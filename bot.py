498d81f71b19d1ba50a63ac8c2c4a87337ebdebc
droid-darkangel <67116649+droid-darkangel@users.noreply.github.com>
2023-11-16 15:50:37 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,19 @@
-from lib import executor, Tg_Bot
-from aiogram import types
+from lib import Tg_Bot, types
 
-token = ''
+token = '5817276802:AAFHbs2EgPLMvJBPkoh4DTReM9o7dlcbUOM'
 
 bot = Tg_Bot(token)
+
+def on_startup():
+    print("I'm working")
 
 
 @bot.dp.message_handler(commands=['start'])
 async def send_start_msg(msg: types.Message):
     print('worked')
     await bot.send_msg(chat_id=msg.from_user.id,
-                       message=f'Hello, my friend {msg.from_user.first_name}'
-                               f'')
+                       message=f'Hello, my friend {msg.from_user.first_name}')
 
 
 if __name__ == "__main__":
-    executor.start_polling(bot.dp, skip_updates=True)
+    bot.start_polling(bot.dp, on_startup=on_startup())
